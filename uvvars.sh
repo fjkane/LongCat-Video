9f2c580ebc30f99b13c4bca8507af62c18f75f57
fjkane <fjkane@protonmail.com>
2025-12-30 16:47:43 +0000

    export vars

--- uvvars.sh
+++ uvvars.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-UV_CACHE="/workspace/cache/uv"
-UV_LINK_MODE=copy
-git config --global user.email "fjkane@protonmail.com"
-git config --global user.name "fjkane"
+export UV_CACHE="/workspace/cache/uv"
+export UV_LINK_MODE=copy
